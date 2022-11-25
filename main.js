77dc203a3729160e026c92c35d8920e057cc2929
tomas cabrera <tomascabrerax@hotmail.com>
2022-11-25 12:28:54 -0300

    simulador interactivo

---
+++ main.js
@@ -0,0 +1,12 @@
+let IngreseUsuario = prompt("Ingrese Usuario")
+let IngreseContraseña = prompt("Ingrese Contraseña"); 
+
+  if (IngreseContraseña =="CabreraTomas"){
+     alert("Contraseña Correcta!");
+}
+else {
+        alert("Contraseña Incorrecta");
+}
+
+alert = alert("Bienvenido a Cabrera Cars");
+confirmacion = confirm("¿Quieres recibir notificaciones?");
